1c41d79ba3eab332055afdaff9c515c7e1ec4899
PUSHPAL <pushpalroy2007@gmail.com>
2019-07-30 18:58:41 +0530

    feat: add recorded ui tests

---
+++ app/src/androidTest/java/com/pushpal/talkie/MainActivityTest.java
@@ -0,0 +1,78 @@
+package com.pushpal.talkie;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.pushpal.talkie.R;
+import com.pushpal.talkie.view.main.MainActivity;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction shineButton = onView(
+                allOf(withId(R.id.btn_like),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.rl_movie_details),
+                                        1),
+                                1),
+                        isDisplayed()));
+        shineButton.perform(click());
+
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.btn_like),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.rl_movie_details),
+                                        1),
+                                1),
+                        isDisplayed()));
+        imageView.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
